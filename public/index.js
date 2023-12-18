b9bcf3f47b525c35c24c05b27e778868eeb4182b
Lucrecia Sabrina Mencia <mencialucre@gmail.com>
2023-12-18 14:17:04 -0300

    creando grid para categorias mas populares

---
+++ public/index.js
@@ -0,0 +1,33 @@
+import Botines from './botines.png';
+import Calza from './calza.png';
+import CardProductoConBoton from './card-producto-con-boton.png';
+import Carrito from './carrito.svg';
+import Frame77 from './Frame77.png';
+import Frame86 from './Frame86.png';
+import Pelota from './pelota.png';
+import Pierce from './Pierce.png';
+import Usuario from './usuario.svg';
+import Vitrina from './vitrina.png';
+import Vtex from './VTEX.png';
+import Zapatilla from './zapatilla.png';
+import Lupa from './lupa.svg';
+import Fire1 from './fire1.svg';
+import Fire2 from './fire2.svg'
+
+export {
+    Botines,
+    Calza,
+    CardProductoConBoton,
+    Carrito,
+    Frame77,
+    Frame86,
+    Pelota,
+    Pierce,
+    Usuario,
+    Vitrina,
+    Vtex,
+    Zapatilla,
+    Lupa,
+    Fire1,
+    Fire2
+};
