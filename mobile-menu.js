a3249c5768084ac3ea28e737e5f467d2d1142eab
Lucrecia Sabrina Mencia <mencialucre@gmail.com>
2023-12-19 22:33:54 -0300

    primera parte de mobile sidebar

---
+++ mobile-menu.js
@@ -0,0 +1,9 @@
+function abrirMenu(){
+    const sidebar = document.querySelector('#sidebar')
+    sidebar.style.display = 'flex'
+}
+
+function cerrarMenu(){
+    const sidebar = document.querySelector('#sidebar')
+    sidebar.style.display = 'none'
+}
